254184d6eaaf608b5d632e528919e04ce976fc6b
Myo Thant <152901402+myothant6@users.noreply.github.com>
2026-04-22 20:43:25 +0630

    Add files via upload

--- game1.c
+++ game1.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <ncurses.h>
 
 int board_x = 5;
 int board_y = 6;
-char board[5][6];
+int menu_select = 0;
+int menu[5] = {1, 2, 3, 4, 5};
+char board[5][6];// a    b    c    d
 char symbols[4] = {'@', '#', '$', '%'};
-//                  a    b    c    d
+bool number_picked = false;
 
 void board_create()
 {
@@ -22,16 +25,64 @@ void board_create()
 
 void print_board()
 {
-    system("clear");
+    clear();
+    
     for (int y = board_y - 1; y >= 0; y--)
     {
         for (int x = 0; x < board_x; x++)
         {
-            printf(" %c ", board[x][y]);
+            if (board[x][y] == symbols[0])
+            {
+                attron(COLOR_PAIR(1));
+                printw(" %c ", board[x][y]);
+                attroff(COLOR_PAIR(1));
+            }
+            else if (board[x][y] == symbols[1])
+            {
+                attron(COLOR_PAIR(2));
+                printw(" %c ", board[x][y]);
+                attroff(COLOR_PAIR(2));
+            }
+            else if (board[x][y] == symbols[2])
+            {
+                attron(COLOR_PAIR(3));
+                printw(" %c ", board[x][y]);
+                attroff(COLOR_PAIR(3));
+            }
+            else if (board[x][y] == symbols[3])
+            {
+                attron(COLOR_PAIR(4));
+                printw(" %c ", board[x][y]);
+                attroff(COLOR_PAIR(4));
+            }
+            else
+            {
+                printw(" %c ", board[x][y]);
+            }
         }
-        printf("\n");
+        printw("\n");
     }
-    printf("---------------\n 1  2  3  4  5 \n");
+    printw("***************\n");
+    
+    // print menu
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == menu_select)
+        {
+            attron(A_REVERSE);
+            printw(" %d ", menu[i]);
+            attroff(A_REVERSE);
+        }
+        else
+        {
+            printw(" %d ", menu[i]);
+        }
+    }
+    
+    printw("\n");
+    (number_picked) ? printw("Enter to Drop\n") : printw("Enter to Pick");
+    
+    refresh();
 }
 
 void init_position()
@@ -85,74 +136,46 @@ void init_position()
     }
 }
 
-int move()
+void player_move(int a, int b)
 {
-    int pick_position, drop_position;
+    int pick_position = a;
+    int drop_position = b;
     
-    printf("q(0): Pick (1-5): ");
-    scanf("%d", &pick_position);
-    
-    if (pick_position == 0) {
-        return 100;
-    }
-    
-    if (pick_position < 0 || pick_position > 5)
+    // check => drop => true => pick
+    for (int i = 0; i < board_y; i++)
     {
-        return 0;
-    }
-    else
-    {
-        printf("Drop (1-5): ");
-        scanf("%d", &drop_position);
-        
-        if (drop_position < 0 || drop_position > 5)
+        if (board[drop_position][i] == '|')
         {
-            return 0;
-        }
-        else
-        {
-            pick_position--;
-            drop_position--;
-            
-            // check => drop => true => pick
-            for (int i = 0; i < board_y; i++)
+            // pick
+            bool found = false;
+            char picking;
+            for (int y = board_y - 1; y >= 0; y--)
             {
-                if (board[drop_position][i] == '|')
+                if (board[pick_position][y] != '|')
                 {
-                    // pick
-                    bool found = false;
-                    char picking;
-                    for (int y = board_y - 1; y >= 0; y--)
-                    {
-                        if (board[pick_position][y] != '|')
-                        {
-                            picking = board[pick_position][y];
-                            board[pick_position][y] = '|';
-                            found = true;
-                            break;
-                        }
-                    }
-                    
-                    if (found)
-                    {
-                        // drop
-                        for (int y = 0; y < board_y; y++)
-                        {
-                            if (board[drop_position][y] == '|')
-                            {
-                                board[drop_position][y] = picking;
-                                break;
-                            }
-                        }
-                    }
-                    
+                    picking = board[pick_position][y];
+                    board[pick_position][y] = '|';
+                    found = true;
                     break;
                 }
             }
+            
+            if (found)
+            {
+                // drop
+                for (int y = 0; y < board_y; y++)
+                {
+                    if (board[drop_position][y] == '|')
+                    {
+                        board[drop_position][y] = picking;
+                        break;
+                    }
+                }
+            }
+            
+            break;
         }
     }
-    
-    return 0;
 }
 
 bool win()
@@ -206,25 +229,60 @@ int main()
     board_create();
     init_position();
     
-    int r;
+    initscr();
+    start_color();
+    use_default_colors();
+    curs_set(0);
+    noecho();
     
+    init_pair(1, COLOR_BLUE, -1);
+    init_pair(2, COLOR_RED, -1);
+    init_pair(3, COLOR_GREEN, -1);
+    init_pair(4, COLOR_YELLOW, -1);
+    
+    int pick_number, drop_number;
     while (true)
     {
         print_board();
-        r = move();
         
-        if (r == 100)
+        int ch = getch();
+        if (ch == 'q')
         {
             break;
+        }
+        else if (ch == 'd')
+        {
+            menu_select = (menu_select + 1) % 5;
+        }
+        else if (ch == 'a')
+        {
+            menu_select = (menu_select - 1 + 5) % 5;
+        }
+        else if (ch == 10)
+        {
+            if (number_picked)
+            {
+                drop_number = menu_select;
+                number_picked = false;
+                player_move(pick_number, drop_number);
+            }
+            else
+            {
+                pick_number = menu_select;
+                number_picked = true;
+            }
         }
         
         if (win())
         {
             print_board();
-            printf("You win! :)\n");
+            printw("\nYou win! :)");
+            refresh();
+            getch();
             break;
         }
     }
     
+    endwin();
     return 0;
 }
